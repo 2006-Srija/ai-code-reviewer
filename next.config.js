09e234f71dafb339f3c2a39610ffccbe21ee4b94
2006-Srija <srijalipi@gmail.com>
2026-03-10 23:01:27 +0530

    Final fix: Clean configuration

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'github.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-    ],
-  },
-  output: 'standalone',
-  trailingSlash: false,
 }
 
 module.exports = nextConfig
